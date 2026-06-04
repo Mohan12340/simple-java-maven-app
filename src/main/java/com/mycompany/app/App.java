8cfcfc5a7435dddeebc96459fa3d4c2f7bbfc971
Mohan <mohan@gmail.com>
2026-06-04 06:24:22 +0000

    WOW

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Welcome!";
+    private static final String MESSAGE = "How are you i am from devops!";
 
     public App() {}
 
